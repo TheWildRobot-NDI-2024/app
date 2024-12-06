01cc3fe8b79f5ba7f4ee103ecf52b1ba0938df1f
Remi Jara <remi.jara4@gmail.com>
2024-12-06 03:14:34 +0100

    Update home page

--- src/script-index.js
+++ src/script-index.js
@@ -38,35 +38,42 @@ const textCoeur = document.getElementById('textCoeur');
 const textPoumons = document.getElementById('textPoumons');
 const textMains = document.getElementById('textMains');
 const textPieds = document.getElementById('textPieds');
-
+let ticking = false;
 window.addEventListener('scroll', function () {
     const scrollPosition = window.scrollY; // position de défilement verticale
 
     // ANIMATION TEXTE APPARITION SCROLL
 
     // Effet de transparence avec un léger mouvement vers le haut
-    const animateText = (element, start, end) => {
-        if (scrollPosition > start && scrollPosition < end) {
-            gsap.to(element, {
-                opacity: 1,
-                y: 0,
-                duration: 0.2,
-                ease: 'power3.out'
-            });
-        } else {
-            gsap.to(element, {
-                opacity: 0,
-                y: 20,
-                duration: 0.2,
-                ease: 'power3.in'
-            });
-        }
-    };
+    if (!ticking) {
+        window.requestAnimationFrame(() => {
+            const animateText = (element, start, end) => {
+                if (scrollPosition > start && scrollPosition < end) {
+                    gsap.to(element, {
+                        opacity: 1,
+                        y: 0,
+                        duration: 1,
+                        ease: 'power3.out'
+                    });
+                } else {
+                    gsap.to(element, {
+                        opacity: 0,
+                        y: 100,
+                        duration: 0.3,
+                        ease: 'power3.in'
+                    });
+                }
+            };
+            console.log(scrollPosition);
+            // Appliquer les animations sur chaque texte
+            animateText(textCerveau, 840, 1100);
+            animateText(textCoeur, 1900, 2300);
+            animateText(textPoumons, 2900, 3300);
+            animateText(textMains, 3900, 4300);
+            animateText(textPieds, 5000, 90000);
 
-    // Appliquer les animations sur chaque texte
-    animateText(textCerveau, 0, 500);
-    animateText(textCoeur, 350, 850);
-    animateText(textPoumons, 550, 950);
-    animateText(textMains, 700, 1250);
-    animateText(textPieds, 900, 1400);
+            ticking = false;
+        });
+        ticking = true;
+    }
 });
